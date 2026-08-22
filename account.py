6923d38f0a8799d9301fe931a8ec0408f237783f
Darksider70-yep <darksider70117@gmail.com>
2026-08-22 14:25:37 +0530

    initial commit

---
+++ account.py
@@ -0,0 +1,9 @@
+class account:
+    def __init__(self, ID, NAME):
+        self.ID = ID     
+        self.NAME = NAME 
+
+
+print("A simple account class for the global Digital bank system.\nThis is a bare bone implementation with minimal functionality.\n\nA simple account class with basic deposit and withdraw functionality.\n\nExpected Output")
+print("=====================================\nGLOBAL DIGITAL BANK - ACCOUNT TEST\n=====================================\n")
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print("A simple account class for the global Digital bank system.\nThis is a bare bone implementation with minimal functionality.\n\nA simple account class with basic deposit and withdraw functionality.\n\nExpected Output")
+print("================================\nGLOBAL DIGITAL BANK - ACCOUNT TEST\n================================\n")
